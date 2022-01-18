269be1891c535cbcceeb79781c562c1e63383ed4
Agne <sanara44@yahoo.com>
2022-01-18 10:37:01 +0200

    Updated

--- src/main/java/com/demo/TestDataReader.java
+++ src/main/java/com/demo/TestDataReader.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 public class TestDataReader {
 
     public static Properties getTestData() throws IOException {
-        FileInputStream fp = new FileInputStream("src\\main\\resources\\testdata.properties");
+        FileInputStream fp = new FileInputStream("/testdata.properties");
         Properties properties = new Properties();
         properties.load(fp);
         return properties;
